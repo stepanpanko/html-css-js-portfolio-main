303d0b99dea99dfc903ce283f954252e17432c2b
stepanpanko <stepanpanko55@gmail.com>
2023-10-10 16:31:12 +0300

    updated

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+window.addEventListener("scroll", function () {
+  var backToTopBtn = document.getElementById("back-to-top-btn");
+
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
